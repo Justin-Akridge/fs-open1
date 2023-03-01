4fc6f8bfcf5d1117fa447af8e3d24fbd32fac823
Justin Akridge <akridjus@gmail.com>
2023-02-28 22:43:47 -0500

    Forms added to page with state management

---
+++ part2/forms/src/App.js
@@ -0,0 +1,133 @@
+import { useState } from "react";
+//import  Note from "./components/Note"
+import Person from "./components/Person";
+
+const App = () => {
+  //add names
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  //add numbers
+  const [numbers, setNumbers] = useState([])
+  const [newNumber, setNewNumber] = useState([])
+
+    const addNumber = (e) => {
+      e.preventDefault()
+      const numberobj = {
+        number: newNumber,
+        id: numbers.length + 1,
+      }
+      setNumbers(numberobj)
+      setNewNumber('')
+    }
+
+    const handleNumber = (e) => {
+      setNewNumber(e.target.value)
+    }
+
+    const addPerson = (e) => {
+      e.preventDefault()
+      console.log(e.target.value)
+      if(e.target.value === undefined){
+        return 
+      }
+      const personobj = {
+        name: newName,
+        id: persons.length + 1,
+      }
+      const isDuplicate = (name) => {
+        for (let i=0; i<persons.length; i++) {
+          if(persons[i].name === name){
+            return true
+          }
+        }
+        return false
+      }
+      if(isDuplicate(newName)){
+        alert(`${newName} already exists`)
+        setNewName('')
+      }
+      else{
+        console.log(newName)
+        setPersons(persons.concat(personobj))
+        setNewName('')
+      }
+    }
+
+    const handlePerson = (e) => {
+      setNewName(e.target.value)
+    }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePerson} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumber} />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      
+        {persons.map(person => {
+          return(
+            <Person key={person.id} person={person} />
+          )
+        })}
+      
+    </div>
+  )
+}
+
+export default App;
+
+// const [ notes, setNotes ] = useState(props.notes)
+//   const [ newNote, setNewNote ] = useState('typing something...')
+//   const [ showAll, setShowAll ] = useState(true)
+
+//   const notesToShow = showAll 
+//     ? notes 
+//     : notes.filter(note => note.important)
+    
+//   const addNote = (e) => {
+//     e.preventDefault()
+//     const noteObject = {
+//       content: newNote,
+//       id: notes.length + 1,
+//       important: Math.random() < .5,
+//     }
+//     setNotes(notes.concat(noteObject))
+//     setNewNote('')
+//   }
+
+//   const handleNoteChange = (e)  => {
+//     setNewNote(e.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <div>
+//         <button onClick={() => setShowAll(!showAll)}>
+//           show {showAll ? 'important' : 'all'}
+//         </button>
+//       </div>
+//       <ul>
+//         {notesToShow.map(note=> {
+//           return (
+//             <Note key={note.id} note={note} />)}
+//           )
+//         }
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input 
+//           value={newNote}
+//           onChange={handleNoteChange}
+//         />
+//         <button type="submit">save</button>
+//       </form>
+//     </div>
+//   );
